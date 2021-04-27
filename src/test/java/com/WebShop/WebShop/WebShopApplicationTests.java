b081ef5931af7bc3f354a0703a0802a8507fc416
Favael <rafaldogoda86@gmail.com>
2021-04-27 18:07:58 +0200

    let's start

---
+++ src/test/java/com/WebShop/WebShop/WebShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WebShop.WebShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
